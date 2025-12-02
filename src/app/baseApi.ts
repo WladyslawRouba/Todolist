0209a48c014ec49d8ac20adb74c261681a557c53
Wladyslaw Rouba <roubovlad@gmail.com>
2025-12-02 18:20:03 +0100

    Fix baseApi: use 1.1 locally, 1.0 for GitHub Pages

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,14 +1,18 @@
 import { AUTH_TOKEN } from "@/common/constants"
 import { handleError } from "@/common/utils"
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-
+const  BASE_URL =
+    import.meta.env.MODE === "production"
+        ? "https://social-network.samuraijs.com/api/1.0/"
+        : "https://social-network.samuraijs.com/api/1.1/"
 export const baseApi = createApi({
   reducerPath: "todolistsApi",
   tagTypes: ["Todolist", "Task","Captcha"] ,
   refetchOnReconnect: true,
   baseQuery: async (args, api, extraOptions) => {
     const result = await fetchBaseQuery({
-      baseUrl: import.meta.env.VITE_BASE_URL,
+      //baseUrl: import.meta.env.VITE_BASE_URL,
+      baseUrl: BASE_URL,
       credentials: 'include',
       headers: {
         "API-KEY": import.meta.env.VITE_API_KEY,
